476db58fd54892150e6e5f773993a47605daa12a
alumno <alumno@E328>
2019-09-06 08:47:42 -0700

    Agregar archivos de proyecto.

---
+++ Herencia/figura.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Herencia
+{
+    abstract class figura
+    {
+        public string Color { get; set; }
+
+        public abstract double calcularArea();
+        
+        public string toString()
+        {
+            return "soy una figura;" + Color;
+        }
+    }
+}
